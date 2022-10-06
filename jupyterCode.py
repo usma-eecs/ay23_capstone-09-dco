c9e7cdb879b69f01370cf23a68afe3e6863f9ff2
MikeGrm <77751650+MikeGrm@users.noreply.github.com>
2022-10-06 10:24:52 -0400

    Update jupyterCode.py
    Can Query and count unique IPs from Alerts

--- jupyterCode.py
+++ jupyterCode.py
@@ -2,23 +2,24 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 import pandas as pd
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-es = Elasticsearch(['https://10.19.89.215:9200'],
+#sets up the environment we are querying from, in this case security onion
+es = Elasticsearch(['https://192.168.6.100:9200'],
 ca_certs=False,verify_certs=False, http_auth=('jupyter','B3@+Navy!!'))
 searchContext = Search(using=es, index='*:so-*', doc_type='doc')
 
-s = searchContext.query('query_string', query='event.module:kibana')
+#This tells our search context what to query, in this example we are doing a query on alerts which are of type event.dataset
+s = searchContext.query('query_string', query='event.dataset:alert')
 
-def query():
-    response = s.execute()
-    if response.success():
+#This creates the dataframe of our query
+response = s.execute()
+if response.success():
     df = pd.DataFrame((d.to_dict() for d in s.scan()))
-    df
+df
 
-def queryWithFiltering():
-    response = s.execute()
-    if response.success():
-        df = pd.DataFrame(([d['metadata'],d['log'],d['type'],d['event'], d['fileset'],d['message']] for d in s))
-    df.columns=['MetaData','Log','Type','Event','FileSet','Message']
-    df
+#The below script will get the unique destination IPs from the dataset and print the count
+df['destination'].value_counts().rename_axis('unique values').to_frame('counts')
+#Similarly this one does it for Source IPs
+df['source'].value_counts().rename_axis('unique values').to_frame('counts')
