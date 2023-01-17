f03e7fa3f25e4ec5e956eb2c8aad39a2aaf9195c
Nicholas Liebers <55268356+FutureCyberMan@users.noreply.github.com>
2023-01-17 11:48:07 -0500

    update with colors
    More needs to be added...this sort of works...

--- Zeek/testCases/testScript.sh
+++ Zeek/testCases/testScript.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "test"
+# Cite: https://linuxhandbook.com/change-echo-output-color/
+# Cite: https://www.geeksforgeeks.org/bash-script-define-bash-variables-and-its-types/
+# Cite: https://www.geeksforgeeks.org/bash-scripting-if-statement/
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+echo "Zeek Scripts"
+echo "  can OS fingerprint"
+echo "          using HTTP"
+echo "                  on Linux"
+echo "                          for Chromium Browsers"
+echo "                                  prints nothing"
+expectedString=""
+result=""
+if [$expectedString == $result]; then
+        echo -e "                                                       ${GREEN}Test Passed!${NOCOLOR}"
+else
+        echo -e "                                                       ${RED}Test Failed!${NOCOLOR}"
+
+fi
