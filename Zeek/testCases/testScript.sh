5ddc569bde4a1541758a84e8f7ff590b69618dd6
Nicholas Liebers <55268356+FutureCyberMan@users.noreply.github.com>
2023-01-19 10:33:29 -0500

    updated helper functions
    This test case scripting framework is now ready to work. I have added the helper functions to make the final implementation more clean

--- Zeek/testCases/testScript.sh
+++ Zeek/testCases/testScript.sh
@@ -9,28 +9,30 @@ NOCOLOR='\033[0m'
 testPassed () {
         local OUTPUT=""
         for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
-        echo -e "$OUTPUT${GREEN}Test Passed!${NOCOLOR}"
+        echo -e "$OUTPUT${GREEN}$2 - Test Passed!${NOCOLOR}"
 }
 testFailed () {
         local OUTPUT=""
         for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
-        echo -e "$OUTPUT${RED}Test Failed!${NOCOLOR}"   
+        echo -e "$OUTPUT${RED}$2 - Test Failed!${NOCOLOR}"      
 }
-testPassed 1
-testFailed 1
 
+addTest () {
+        local OUTPUT=""
+        for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
+        echo -e "$OUTPUT$2"
+}
 
-echo "Zeek Scripts"
-echo "  can OS fingerprint"
-echo "          using HTTP"
-echo "                  on Linux"
-echo "                          for Chromium Browsers"
-echo "                                  prints nothing"
+addTest 0 "Zeek Scripts"
+addTest 1 "can OS fingerprint"
+addTest 2 "using HTTP"
+addTest 3 "on Linux"
+addTest 4 "for Chromium Browsers"
+addTest 5 "blank string"
 expectedString=""
 result=""
 if [$expectedString == $result]; then
-        echo -e "                                                       ${GREEN}Test Passed!${NOCOLOR}"
+        testPassed 5 "blank string"
 else
-        echo -e "                                                       ${RED}Test Failed!${NOCOLOR}"
-
+        testFailed 5 "blank string"
 fi
