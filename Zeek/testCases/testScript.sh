576a30c028698e32a248f1f50e5281c688ed7d1a
Nicholas Liebers <55268356+FutureCyberMan@users.noreply.github.com>
2023-01-24 09:36:00 -0500

    small changes to use variables
    I used some variables instead of creating magic numbers. Helps keep the overall flow and be easier to read.

--- Zeek/testCases/testScript.sh
+++ Zeek/testCases/testScript.sh
@@ -7,60 +7,65 @@ GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
 testPassed () {
-        local OUTPUT=""
-        for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
-        echo -e "$OUTPUT${GREEN}$2 - Test Passed!${NOCOLOR}"
+	local OUTPUT=""
+	for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
+	echo -e "$OUTPUT${GREEN}$2 - Test Passed!${NOCOLOR}"
 }
 testFailed () {
-        local OUTPUT=""
-        for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
-        echo -e "$OUTPUT${RED}$2 - Test Failed!${NOCOLOR}"      
+	local OUTPUT=""
+	for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
+	echo -e "$OUTPUT${RED}$2 - Test Failed!${NOCOLOR}"	
 }
 
 addTest () {
-        local OUTPUT=""
-        for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
-        echo -e "$OUTPUT$2"
+	local OUTPUT=""
+	for ((i = 0; i < $1; i++)); do OUTPUT+="\t"; done
+	echo -e "$OUTPUT$2"
 }
-
 addTest 0 "Zeek Scripts"
 addTest 1 "can OS fingerprint"
-addTest 2 "using HTTP"
-addTest 3 "on Linux"
+addTest 2 "on Windows"
+addTest 2 "on Linux"
+addTest 3 "using HTTP"
 addTest 4 "for Chromium Browsers"
-addTest 5 "Manual String"
-expectedString=""
-result=""
+num=5
+addTest $num "Manual String"
+expectedString="Linux x86_64 Chromium 107.0.0.0"
+result="Linux x86_64 Chromium 107.0.0.0"
 # CITE: https://stackoverflow.com/questions/19733437/getting-command-not-found-error-while-comparing-two-strings-in-bash
 if [[ $expectedString == $result ]]; then
-        testPassed 5 "Chromium Linux"
+	testPassed $num "Chromium Linux"
 else
-        testFailed 5 "Chromium Linux"
+	testFailed $num "Chromium Linux"
 fi
-addTest 5 "Chromium Linux"
+num=5
+addTest $num "Chromium Linux"
 expectedString="Linux x86_64 Chromium 107.0.0.0"
 result="$(zeek -C -r ../../httpWebsiteLinux.pcap -B all frameworks/software/httpOSDetect.zeek)"
 if [[ $expectedString == $result ]]; then
-        testPassed 5 "Chromium Linux"
+	testPassed $num "Chromium Linux"
 else
-        testFailed 5 "Chromium Linux"
+	testFailed $num "Chromium Linux"
 fi
 
 addTest 4 "for FireFox Browser"
-addTest 5 "Manual String"
+num=5
+addTest $num "Manual String"
 expectedString="Ubuntu Linux x86_64 Firefox 108.0"
 result="Ubuntu Linux x86_64 Firefox 108.0"
 if [[ $expectedString == $result ]]; then
-        testPassed 5 "Firefox Linux"
+	testPassed $num "Firefox Linux"
 else
-        testFailed 5 "Firefox Linux"
+	testFailed $num "Firefox Linux"
 fi
 
 addTest 5 "Firefox Linux"
 epectedString="Ubuntu Linux x86_64 Firefox 108.0"
 result="$(zeek -C -r ../../httpWebsiteLinuxFirefox.pcap -B all frameworks/software/httpOSDetect.zeek | head -n 1)"
 if [[ $expectedString == $result ]]; then
-        testPassed 5 "Firefox Linux"
+	testPassed $num "Firefox Linux"
 else
-        testFailed 5 "Firefox Linux"
+	testFailed $num "Firefox Linux"
 fi
+
+addTest 3 "On Windows"
